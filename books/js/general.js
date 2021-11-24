42ce21aabb0298ef481b2b2a62adf6d74c4560a9
Ibnussabel <traduim@gmail.com>
2021-11-24 19:26:22 +0100

    test 4

--- books/js/general.js
+++ books/js/general.js
@@ -4,20 +4,22 @@ fetch('./assets/libros.json')
 .then(jsonResponse => {
   const json_libros = jsonResponse;
   console.log("json_libros:"+json_libros);
-});
-const libros = JSON.parse(json_libros);
-console.log("libros:"+libros);
+})
+.then(
+    const libros = JSON.parse(json_libros);
+    console.log("libros:"+libros);
+).then(
+    let libreria = "";
+    for(var i = 0; i < libros.length; i++) {
+        let libro = libros[i];
+        libreria += '<article>';
+        libreria += '<img src="'+libro.portada+'" alt="'+libro.titulo+'. '+libro.autor+', '+libro.anyo+'">';
+        libreria += '<p><strong class="title">'+libro.titulo+'</strong><br><span class="author">'+libro.autor+'</span><br><span class="flag">'+getFlagEmoji(libro.pais)+'</span> <span class="year">'+libro.anyo+'</span></p>';
+        libreria += '</article>';
+    }
 
-let libreria = "";
-for(var i = 0; i < libros.length; i++) {
-    let libro = libros[i];
-    libreria += '<article>';
-    libreria += '<img src="'+libro.portada+'" alt="'+libro.titulo+'. '+libro.autor+', '+libro.anyo+'">';
-    libreria += '<p><strong class="title">'+libro.titulo+'</strong><br><span class="author">'+libro.autor+'</span><br><span class="flag">'+getFlagEmoji(libro.pais)+'</span> <span class="year">'+libro.anyo+'</span></p>';
-    libreria += '</article>';
-}
-
-document.getElementById('shelf').innerHTML = libreria;
+    document.getElementById('shelf').innerHTML = libreria;
+);
 
 const filterCountry = countryCode => {
     let numBooks = 0;
