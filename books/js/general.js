2b8d27e0e8a148ecb84b426afb860f82d03daf3c
Ibnussabel <traduim@gmail.com>
2022-01-04 18:31:20 +0100

    Debug

--- books/js/general.js
+++ books/js/general.js
@@ -2,18 +2,19 @@ var books;
 var writers;
 var countries;
 
-const showcase = (books, writers, countries) => {
+const showcase = (books) => {
     let shelf = '';
     for(let i = 0; i < books.length; i++) {
         let book = books[i];
         let author = writers.find(person => person.name == book.writer);        
+        console.log(i);
+        console.log(author);
         shelf += '<article>';
         shelf += '<img class="cover" src="'+book.cover+'" alt="'+book.name+'. '+book.writer+', '+book.published+'">';
         shelf += '<p><strong class="title">'+book.name+'</strong><br><span class="author">'+book.writer+'</span><br>';
-        shelf += '<img class="flag" src="./img/flags/'+author['country'].toLowerCase()+'.png"> <span class="year">'+book.published+'</span></p>';
+        shelf += '<img class="flag" src="./img/flags/'+author.country.toLowerCase()+'.png"> <span class="year">'+book.published+'</span></p>';
         shelf += '</article>';
     }
-    console.log(author);
     document.getElementById('shelf').innerHTML = shelf;    
 }
 
@@ -34,5 +35,5 @@ fetch('./assets/books.json')
   .then(data => {
     books = JSON.parse(JSON.stringify(data));
 
-    showcase(books, writers, countries);
+    showcase(books);
   })
