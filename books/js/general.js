685f4857a1eb30ba073c277c8c9e2222cfd84d17
Ibnussabel <traduim@gmail.com>
2022-01-04 18:43:27 +0100

    Refactor

--- books/js/general.js
+++ books/js/general.js
@@ -3,17 +3,19 @@ var writers;
 var countries;
 
 const showcase = (books) => {
+    let book, author, country, gender;
     let shelf = '';
-    let book;
-    let author;
+
     for(let i = 0; i < books.length; i++) {
         book = books[i];
         author = writers.find(person => person.name == book.writer);        
-        console.log(author.country);
+        gender = author.gender;
+        country = author.country;
+
         shelf += '<article>';
         shelf += '<img class="cover" src="'+book.cover+'" alt="'+book.name+'. '+book.writer+', '+book.published+'">';
-        shelf += '<p><strong class="title">'+book.name+'</strong><br><span class="author">'+book.writer+'</span><br>';
-        shelf += '<img class="flag" src="./img/flags/'+author.country.toLowerCase()+'.png"> <span class="year">'+book.published+'</span></p>';
+        shelf += '<p><strong class="title">'+book.name+'</strong><br><span class="author '+gender+'">'+book.writer+'</span><br>';
+        shelf += '<img class="flag" src="./img/flags/'+country.toLowerCase()+'.png"> <span class="year">'+book.published+'</span></p>';
         shelf += '</article>';
     }
     document.getElementById('shelf').innerHTML = shelf;    
@@ -23,18 +25,18 @@ fetch('./assets/writers.json')
   .then(response => response.json())
   .then(data => {
     writers = JSON.parse(JSON.stringify(data));
-  })
 
-fetch('./assets/countries.json')
-  .then(response => response.json())
-  .then(data => {
-    countries = JSON.parse(JSON.stringify(data));
+  fetch('./assets/countries.json')
+    .then(response => response.json())
+    .then(data => {
+      countries = JSON.parse(JSON.stringify(data));
+
+  fetch('./assets/books.json')
+    .then(response => response.json())
+    .then(data => {
+      books = JSON.parse(JSON.stringify(data));
+
+      showcase(books);
+    })
+  })
 })
-
-fetch('./assets/books.json')
-  .then(response => response.json())
-  .then(data => {
-    books = JSON.parse(JSON.stringify(data));
-
-    showcase(books);
-  })
